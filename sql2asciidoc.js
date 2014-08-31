9cffc611c8f889fd74db16eaf12f5a904428affe
Wilfried Kopp <chevdor@gmail.com>
2014-08-31 10:50:30 +0200

    Cleaning up the code.

--- sql2asciidoc.js
+++ sql2asciidoc.js
@@ -47,15 +47,14 @@ var ops = stdio.getopt({
 
 });
 
-//console.log(ops);
 var config = {
     user     : ops['login'],
     password : ops['pass'],
-    server   : ops['server'], // You can use 'localhost\\instance' to connect to named instance
+    server   : ops['server'],       // You can use 'localhost\\instance' to connect to named instance
     database : ops['database'],
 
     options: {
-        encrypt: false // Use this if you're on Windows Azure
+        encrypt: false              // Use this if you're on Windows Azure
     }
 }
 
@@ -63,7 +62,7 @@ var config = {
 var co = new sql.Connection(config, function(err) {
     //console.info('connected!');
     // ... error checks
-    if (err) console.log(err);
+    if (err) throw (err);
 
     var rq1 = new sql.Request(co);
     rq1.multiple = true;
@@ -74,13 +73,11 @@ var co = new sql.Connection(config, function(err) {
 
     q_schemas += " ORDER BY SCHEMA_NAME ASC ";
 
-    rq1.on('done', function(retVal) {
-        console.info('DONE MAIN');
-    });
+    // rq1.on('done', function(retVal) {
+    //     console.info('DONE MAIN');
+    // });
 
     rq1.query(q_schemas, function(err, schemas) {
-        if (err) console.log(err);
-        
         //console.info("Schema found: " + schemas[0].length);
 
         var lastSchema = "";
@@ -100,7 +97,6 @@ var co = new sql.Connection(config, function(err) {
                     q_colums += "WHERE TABLE_SCHEMA='" + schema + "' ";
                     q_colums += "AND TABLE_NAME ='" + table + "'";
 
-                    //console.log(q_colums);
                     var res = [];
                     rq1.query(q_colums, function(err, cols) {
                         //console.log(cols[0]);               // here we have an array of columns for a given schema.table
@@ -118,7 +114,6 @@ var co = new sql.Connection(config, function(err) {
                         var line = [];
 
                         cols[0].forEach(function(col) {
-                            //console.log(col);
                             //console.log(schema + '.' + table + '.' + col['COLUMN_NAME']);
 
                             for (var colprop in col) {
@@ -127,22 +122,16 @@ var co = new sql.Connection(config, function(err) {
                                     'CHARACTER_MAXIMUM_LENGTH',
                                     'IS_NULLABLE'
                                 ].indexOf(colprop) >= 0) {
-                                    //console.log('found column prop we want:' + colprop)
-                                    //console.log(col[colprop]);
                                     line.push('| ' + col[colprop]);
-                                    //console.log(line);
                                 }
                             }
                             line.push('\n');
-                            //console.log(line);
-                            //console.log('TZ');
                         });
                         line.pop();
 
                         res.push(line.join(''));
                         res.push('|==== ');
                         console.log(res.join('\n'));
-                        //console.log('After each table');
                     });
                 });
             });
